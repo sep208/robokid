fefe314f1ca0aa9d18150f8e86dd5cd7481a17c3
sep208 <70412292+sep208@users.noreply.github.com>
2021-03-21 16:05:11 -0400

    Create cardArray.js

---
+++ cardArray.js
@@ -0,0 +1,24 @@
+  
+import React from 'react';
+import card from './card';
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default CardList;
